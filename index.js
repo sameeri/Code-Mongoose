a07b7d269ddbcb1020ddb655c81f18759aee47e9
sameeri <smarryboyina@telogical.com>
2016-03-06 13:04:03 -0600

    Working with ES6 promises (#3)

--- index.js
+++ index.js
@@ -1,95 +1,82 @@
 'use strict';
 
-// Inspect: what is on mongoose
 var mongoose = require('mongoose');
-console.log('The mongoose library', mongoose);
-
-
-// Inspect: what is the Schema constructor function
 var Schema = mongoose.Schema;
-console.log('The schema ctor fn', Schema);
 
-var schemaSpec = {
+//Set mongoose to work with ES6 promises
+
+mongoose.Promise = Promise;
+
+
+//Routine work:
+// Setup a connection
+// create a schema definition
+// create a model from the schema defined
+
+var languageSchemaDefinition = {
   'name': String
 };
 
-// Inspect: an object created by the schema constructor
-var bookSchema = new Schema(schemaSpec);
-console.log('Books Schema', bookSchema);
+var languageSchema = new Schema(languageSchemaDefinition);
+
+var LanguageModel = mongoose.model('Languages', languageSchema);
 
 
-// Inspect: the model helper method.
-console.log('The model helper method', mongoose.model)
-
-// Create a model object and inspect it
-// The model method create a constructor function and returns it.
-// Using this constructor function we can now create model objects.
-// Treat the returned function as a class. You can instantiate/operate on the class now.
-var BookModel = mongoose.model('Book', bookSchema);
-console.log('What is the model object returned by model?', BookModel);
-
-
-// Create a model instance
-var theGoodParts = new BookModel({
-  'name': 'JavaScript: The Good Parts'
-});
-
-// Inspect : The model instance
-console.log("What's the good parts?", theGoodParts);
-
-// Create a connection object.
-mongoose.connect('mongodb://localhost/books');
+mongoose.connect('mongodb://localhost/languages');
 var db = mongoose.connection;
-
-//Inspect the connection object
-console.log("What's the connection object?", db);
-
 db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function() {
+db.once('open', onOpen);
+
+function onOpen() {
   console.log('Yay We are connected');
 
-  //Save a doc.
-  // Model instances have instance methods that lets you perform saves.
-  theGoodParts.save(function(err, fluffy) {
-    if (err) return console.error(err);
-    console.log('the doc was saved successfully');
-
-    //Read the doc, after it gets saved. Here you have to access the Model. Because the model represents the collection.
-    BookModel.find(function(err, books) {
-      if (err) {
-        console.log(err);
-      }
-      console.log("Books after saving the good parts", books);
-      //We can also use Model's create method to create a document.
-      BookModel.create({
-        'name': 'JavaScript The definitive guide'
-      }, function(err, books) {
-        if (err) {
-          console.log(err);
-        }
-        console.log("Books after saving the definitive guide", books);
-
-        //Remove all and read again.
-        BookModel.remove(function(err) {
-          if (err) return handleError(err);
-          // Now reading should give us back 0 docs.
-          BookModel.find(function(err, books) {
-            if (err) {
-              console.log(err);
-            }
-            console.log("Books after remove all", books);
-          })
-        });
-      });
-
-    })
-
+  LanguageModel.find().then(function onSuccess(docs) {
+    console.log('Yay, we got docs', docs);
+  }, function onError(err) {
+    console.log('error reading docs', err);
   });
 
+  var javaScript = {
+    'name': 'JavaScript'
+  };
+
+  LanguageModel.create(javaScript).then(
+    function onCreateSuccess(docs) {
+      console.log('Yay, we created docs', docs);
+
+      //Let's read again
+      LanguageModel.find().then(
+        function onSuccess(docs) {
+          console.log('Yay, we got docs', docs);
+
+          var doc = docs[0];
+          console.log("What's the doc bro?", doc);
 
 
+                //Find based on id.
+                LanguageModel.findById(doc._id).then(
+                  function onSuccess(docs) {
+                      console.log('Yay, we got a doc by its id', docs);
+                      //Remove based on id now :)
+                      LanguageModel.findByIdAndRemove(doc.id).then(
+                      function onSuccess(docs) {
+                            console.log('Yay, we removed a doc by its id', docs);
+                          },
+                        function onError(err) {
+                          console.log('error reading doc by id', err);
+                        }
+                      );
+                    });
+        },
+        function onError(err) {
+          console.log('error reading docs', err);
+        }
+      );
+    },
+    function onCreateError(err) {
+      console.log('error creating doc', err);
+    }
+  );
 
-
-
-});
+}
